d651364e180ce2ff48dee81dd597635f8fa3c3b6
isaac <isaacd4444@gmail.com>
2022-09-24 20:00:05 -0400

    changed files to php

--- feedback.php
+++ feedback.php
@@ -16,19 +16,15 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+           <a class="nav-link" href="./index.php">Home</a>
+         </li>
          <li class="nav-item">
-              <a class="nav-link" href="./index.html">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./feedback.html"
-                >Feedback</a
-              >
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./about.html"
-                >About</a
-              >
-            </li>
+           <a class="nav-link" href="./feedback.php">Feedback</a>
+         </li>
+         <li class="nav-item">
+           <a class="nav-link" href="./about.php">About</a>
+         </li>
         </ul>
       </div>
   </div>
@@ -36,27 +32,27 @@
 
 <main>
   <div class="container d-flex flex-column align-items-center">
-    <img src="/php-crash/feedback/img/logo.png" class="w-25 mb-3" alt="">
+   
     <h2>Feedback</h2>
-    <p class="lead text-center">Leave feedback for Traversy Media</p>
-    <form action="" class="mt-4 w-75">
-      <div class="mb-3">
-        <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
-      </div>
-      <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
-      </div>
-      <div class="mb-3">
-        <label for="body" class="form-label">Feedback</label>
-        <textarea class="form-control" id="body" name="body" placeholder="Enter your feedback"></textarea>
-      </div>
-      <div class="mb-3">
-        <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
-      </div>
-    </form>
-    </div>
+
+    <div class="card my-3">
+     <div class="card-body">
+       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+     </div>
+   </div>
+
+   <div class="card my-3">
+     <div class="card-body">
+       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+     </div>
+   </div>
+
+   <div class="card my-3">
+     <div class="card-body">
+       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+     </div>
+   </div>
+  </div>
 </main>
 
 <footer class="text-center mt-5">
--- index.php
+++ index.php
@@ -17,14 +17,18 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
          <li class="nav-item">
-           <a class="nav-link" href="./index.html">Home</a>
-         </li>
-         <li class="nav-item">
-           <a class="nav-link" href="./feedback.html">Feedback</a>
-         </li>
-         <li class="nav-item">
-           <a class="nav-link" href="./about.html">About</a>
-         </li>
+              <a class="nav-link" href="./index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./feedback.php"
+                >Feedback</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./about.php"
+                >About</a
+              >
+            </li>
         </ul>
       </div>
   </div>
@@ -32,9 +36,26 @@
 
 <main>
   <div class="container d-flex flex-column align-items-center">
-    <h2>About</h2>
-
-    <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore impedit totam porro iure reiciendis autem possimus sapiente, optio, exercitationem ipsum assumenda mollitia, recusandae expedita culpa ratione voluptatem esse quos quam?</p>
+    <img src="./img/cool-dj.jpg" class="w-25 mb-3" alt="">
+    <h2>Feedback</h2>
+    <p class="lead text-center">Leave feedback for Traversy Media</p>
+    <form action="" class="mt-4 w-75">
+      <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+      </div>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
+      </div>
+      <div class="mb-3">
+        <label for="body" class="form-label">Feedback</label>
+        <textarea class="form-control" id="body" name="body" placeholder="Enter your feedback"></textarea>
+      </div>
+      <div class="mb-3">
+        <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
+      </div>
+    </form>
     </div>
 </main>
 
