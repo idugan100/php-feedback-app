de960935ada4eb3d1ade946b6ae7d7a11d71689a
isaac <isaacd4444@gmail.com>
2022-09-24 22:03:44 -0400

    post new feedback

--- index.php
+++ index.php
@@ -11,13 +11,35 @@
     else{
       $name=filter_input(INPUT_POST,'name',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     }
+
     if(empty($_POST['email'])){
       $emailErr="Email is required";
     }else{
-      $email=filter_input(INPUT_POST,'email',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+      $email=filter_input(INPUT_POST,'email',FILTER_SANITIZE_EMAIL);
     }
-
+    if(empty($_POST['body'])){
+      $bodyErr="Feedback is required";
+    }
+    else{
+      $body=filter_input(INPUT_POST,'body',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    }
+    
+    if(empty($nameErr) && empty($emailErr) && empty($bodyErr)){
+      //add to database
+      $sql="INSERT INTO feedback (name,email,body)VALUES ('$name','$email','$body')";
+      if(mysqli_query($conn,$sql)){
+        //sucess
+        header("Location: feedback.php");
+      }else{
+        echo 'Error'. mysqli_error($conn);
+      }
+    }
+   
   }
+
+    
+
+  
 ?>
 
     <img src="./img/cool-dj.jpg" class="w-25 mb-3" alt="">
@@ -26,23 +48,32 @@
 
     <p class="lead text-center">Leave feedback for Traversy Media</p>
 
-    <form action="<?php echo htmlspecialchars($_Server['PHP_SELF']) ;?> method="POST"class="mt-4 w-75">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ;?>" method="POST"class="mt-4 w-75">
 
-      <div class="mb-3">
+    <div class="mb-3">
         <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+        <input type="text" class="form-control <?php echo !$nameErr ?:
+          'is-invalid'; ?>" id="name" name="name" placeholder="Enter your name" >
+        <div id="validationServerFeedback" class="invalid-feedback">
+          <?php echo $nameErr;?>
+        </div>
       </div>
-
       <div class="mb-3">
         <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
-      </div>
-
+        <input type="email" class="form-control <?php echo !$emailErr ?:
+          'is-invalid'; ?>" id="email" name="email" placeholder="Enter your email" >
+          <div id="validationServerFeedback" class="invalid-feedback">
+          <?php echo $emailErr;?>
+        </div>  
+    </div>
       <div class="mb-3">
         <label for="body" class="form-label">Feedback</label>
-        <textarea class="form-control" id="body" name="body" placeholder="Enter your feedback"></textarea>
-      </div>
-      
+        <textarea class="form-control <?php echo !$bodyErr ?:
+          'is-invalid'; ?>" id="body" name="body" placeholder="Enter your feedback"></textarea>
+          <div id="validationServerFeedback" class="invalid-feedback">
+          <?php echo $bodyErr;?>
+        </div>  
+    </div>
       <div class="mb-3">
         <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
       </div>
